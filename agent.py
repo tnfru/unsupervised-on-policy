abd735535069ac6b5e25d521bdb4f063d16cef47
Lars Müller <lars.lmueller@gmail.com>
2021-08-16 19:09:59 +0200

    refactor: aux training phase

--- agent.py
+++ agent.py
@@ -1,4 +1,3 @@
-import torch
 import torch as T
 
 import torch.nn as nn
@@ -8,10 +7,10 @@ from torch.distributions.categorical import Categorical
 
 from networks import PPG, CriticNet
 from logger import *
-from utils import approx_kl_div
+from utils import approx_kl_div, data_to_device
 from trajectory import Trajectory
-from utils import data_to_device
-from utils import value_loss_fun
+from objectives import ppo_objective, value_loss_fun, entropy_objective
+from aux_train import train_aux_epoch
 
 
 class Agent:
@@ -20,7 +19,6 @@ class Agent:
         self.actor = PPG(action_dim, state_dim)
         self.critic = CriticNet(state_dim)
         self.batch_size = config['batch_size']
-        self.policy_clip = config['clip_ratio']
         self.actor_opt = optim.Adam(self.actor.parameters(),
                                     lr=config['actor_lr'])
         self.critic_opt = optim.Adam(self.critic.parameters(),
@@ -49,31 +47,54 @@ class Agent:
 
             return action.item(), log_prob, aux_value.item(), log_dist
 
+    def learn(self):
+        config = self.config
+        for epoch in range(config['train_iterations']):
+            loader = DataLoader(self.trajectory, batch_size=config[
+                'batch_size'], shuffle=True, pin_memory=True)
+            self.train_ppo_epoch(loader)
+            self.entropy_coeff *= config['entropy_decay']
+            self.steps += 1
+
+        if self.steps == config['aux_freq']:
+            self.aux_training_phase()
+
+    def aux_training_phase(self):
+        config = self.config
+        self.trajectory.is_aux_epoch = True
+        self.steps = 0
+        loader = DataLoader(self.trajectory, batch_size=config[
+            'batch_size'], shuffle=True, pin_memory=True)
+
+        for aux_epoch in range(config['aux_iterations']):
+            train_aux_epoch(agent=self, loader=loader)
+        self.trajectory.is_aux_epoch = False
+
     def train_ppo_epoch(self, loader):
         for rollout_data in loader:
             states, actions, expected_returns, state_vals, advantages, \
             log_probs = data_to_device(rollout_data, self.device)
             expected_returns = expected_returns.unsqueeze(1)
 
-            self.train_policy_net(states, actions, log_probs, advantages)
+            self.train_policy(states, actions, log_probs, advantages)
             self.train_critic(states, expected_returns, state_vals)
 
-    def train_policy_net(self, states, actions, old_log_probs, advantages):
+    def train_policy(self, states, actions, old_log_probs, advantages):
         config = self.config
         action_probs, _ = self.actor(states)
         action_dist = Categorical(logits=action_probs)
         log_probs = action_dist.log_prob(actions)
 
-        entropy_loss = -action_dist.entropy().mean() * self.entropy_coeff
         # entropy for exploration
+        entropy_loss = entropy_objective(action_dist, self.entropy_coeff)
 
         # log trick for efficient computational graph during backprop
         ratio = T.exp(log_probs - old_log_probs)
+        ppo_loss = ppo_objective(advantages, ratio, config['policy_clip'])
+
+        objective = ppo_loss + entropy_loss
+
         kl_div = approx_kl_div(log_probs, old_log_probs, ratio)
-
-        ppo_objective = self.calculate_ppo_objective(advantages, ratio)
-        objective = ppo_objective + entropy_loss
-
         if kl_div < config['kl_max']:
             # If KL divergence is too big we don't take gradient steps
             self.do_gradient_step(self.actor, self.actor_opt, objective,
@@ -91,71 +112,6 @@ class Agent:
                                      config['grad_norm'])
         objective.backward(retain_graph=retain_graph)
         optimizer.step()
-
-    def calculate_ppo_objective(self, advantages, ratio):
-        weighted_objective = ratio * advantages
-        clipped_objective = ratio.clamp(1 - self.policy_clip,
-                                        1 + self.policy_clip) * advantages
-        ppo_objective = -T.min(weighted_objective,
-                               clipped_objective).mean()
-        return ppo_objective
-
-    def learn(self):
-        config = self.config
-        for epoch in range(config['train_iterations']):
-            loader = DataLoader(self.trajectory, batch_size=config[
-                'batch_size'], shuffle=True, pin_memory=True)
-            self.train_ppo_epoch(loader)
-            self.entropy_coeff *= config['entropy_decay']
-            self.steps += 1
-
-        if self.steps == config['aux_freq']:
-            self.steps = 0
-            loader = DataLoader(self.trajectory, batch_size=config[
-                'batch_size'], shuffle=True, pin_memory=True)
-
-            for aux_epoch in range(config['aux_iterations']):
-                self.train_aux_epoch(loader)
-
-    def train_aux_epoch(self, loader):
-        self.trajectory.is_aux_epoch = True
-
-        for rollout_data in loader:
-            states, expected_returns, aux_rets, state_vals, aux_vals, \
-            log_dists = data_to_device(rollout_data, self.device)
-            expected_returns = expected_returns.unsqueeze(1)
-            aux_rets = aux_rets.unsqueeze(1)
-
-            self.train_aux_net(states, aux_rets, log_dists, aux_vals)
-            self.train_critic(states, expected_returns, state_vals)
-
-        self.trajectory.is_aux_epoch = False
-
-    def train_aux_net(self, states, expected_returns, old_log_probs,
-                      old_aux_value):
-        config = self.config
-        action_probs, aux_values = self.actor(states)
-        action_dist = Categorical(logits=action_probs)
-        kl_div = approx_kl_div(action_dist.probs.log(), old_log_probs)
-
-        aux_value_loss = value_loss_fun(state_values=aux_values,
-                                        old_state_values=old_aux_value,
-                                        expected_returns=expected_returns,
-                                        is_aux_epoch=True,
-                                        value_clip=config['value_clip'])
-        aux_value_loss *= config['val_coeff']
-
-        if aux_value_loss > self.AUX_WARN_THRESHOLD:
-            warn_about_aux_loss_scaling(aux_value_loss)
-
-        aux_loss = aux_value_loss + kl_div * config['beta']
-
-        if kl_div < config['kl_max']:
-            # If KL divergence is too big we don't take gradient steps
-            self.do_gradient_step(self.actor, self.actor_opt, aux_loss)
-
-        if self.use_wandb:
-            log_aux(aux_values, aux_loss, kl_div, config['kl_max'])
 
     def train_critic(self, states, expected_returns, old_state_values):
         config = self.config
--- utils.py
+++ utils.py
@@ -1,30 +1,4 @@
 import torch as T
-import torch.nn.functional as F
-from torch.distributions.categorical import Categorical
-
-
-def clipped_value_loss(state_values, old_state_values, expected_returns, clip):
-    clipped_difference = (state_values - old_state_values).clamp(-clip, clip)
-    value_clipped = old_state_values + clipped_difference
-
-    clipped_loss = T.square(value_clipped - expected_returns)
-    mse_loss = T.square(state_values - expected_returns)
-
-    loss = T.max(clipped_loss, mse_loss).mean()
-
-    return loss
-
-
-def value_loss_fun(state_values, old_state_values, expected_returns,
-                   is_aux_epoch, value_clip):
-    if value_clip is not None and is_aux_epoch:
-        # clip value loss in aux episodes to reduce overfitting
-        loss = clipped_value_loss(state_values, old_state_values,
-                                  expected_returns, value_clip)
-    else:
-        loss = F.mse_loss(state_values, expected_returns)
-
-    return loss
 
 
 def calculate_advantages(rewards, state_vals, discount_factor, gae_lambda):
@@ -42,7 +16,7 @@ def calculate_advantages(rewards, state_vals, discount_factor, gae_lambda):
 
 
 def approx_kl_div(log_probs, old_log_probs, ratio=None):
-    # See Josh Schulamns Blogpost http://joschu.net/blog/kl-approx.html
+    # See Josh Schulmans Blogpost http://joschu.net/blog/kl-approx.html
     with T.no_grad():
         if ratio is None:
             ratio = T.exp(log_probs - old_log_probs)
