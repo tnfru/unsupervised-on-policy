667e1f0affbb67a4823fb0a34abdbf6d5d40a207
Lars Müller <lars.lmueller@gmail.com>
2022-01-28 19:52:50 +0100

    remove: model saving feedback

--- ppg/agent.py
+++ ppg/agent.py
@@ -135,7 +135,6 @@ class Agent(T.nn.Module):
         os.makedirs(self.path, exist_ok=True)
         PATH = self.path + '/agent_latest.pt'
         T.save(self.state_dict(), PATH)
-        print(f'Model saved at {PATH}')
 
     def load_model(self):
         PATH = self.path + '/agent_latest.pt'
