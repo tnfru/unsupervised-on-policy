c4d4567031d745ad7906564a9176e4514e0c90d2
Lars Müller <lars.lmueller@gmail.com>
2022-01-28 18:48:25 +0100

    refactor: trajectory, rollout

--- ppg/trajectory.py
+++ ppg/trajectory.py
@@ -1,12 +1,15 @@
 import torch as T
 
+from ppg.gae import calculate_advantages
+from utils.network_utils import normalize
+
 
 class Trajectory(T.utils.data.Dataset):
     def __init__(self):
-        #TODO call super
         self.states = []
-        self.log_probs = []
         self.actions = []
+        self.rewards = []
+        self.log_probs = []
         self.expected_returns = []
         self.dones = []
         self.advantages = []
@@ -19,26 +22,49 @@ class Trajectory(T.utils.data.Dataset):
     def __len__(self):
         return len(self.states)
 
-    def append_timesteps(self, states, actions, expected_returns, dones,
-                         log_probs, advantages, aux_vals, log_dists,
-                         state_vals, aux_rets):
-        self.states.extend(states)
-        self.actions.extend(actions)
-        self.expected_returns.extend(expected_returns)
-        self.dones.extend(dones)
-        self.log_probs.extend(log_probs)
-        self.advantages.extend(advantages)
-        self.aux_state_values.extend(aux_vals)
-        self.log_dists.extend(log_dists)
-        self.state_vals.extend(state_vals)
-        self.aux_rets.extend(aux_rets)
+    def append_step(self, state, state_val, action, reward, done,
+                    log_prob, aux_val, log_dist):
+        self.states.append(state)
+        self.actions.append(action)
+        self.dones.append(done)
+        self.log_probs.append(log_prob)
+        self.aux_state_values.append(aux_val)
+        self.state_vals.append(state_val)
+        self.log_dists.append(log_dist)
+        self.rewards.append(reward)
 
-    def fix_datatypes(self):
+    def data_to_tensors(self):
         self.states = T.cat(self.states)
         self.actions = T.tensor(self.actions, dtype=T.long)
         self.dones = T.tensor(self.dones, dtype=T.int)
         self.log_probs = T.tensor(self.log_probs, dtype=T.float)
         self.log_dists = T.stack(self.log_dists)
+
+    def calc_advantages(self, config):
+        advantages = calculate_advantages(
+            self.rewards,
+            self.state_vals,
+            self.dones,
+            config['discount_factor'],
+            config['gae_lambda']
+        )
+        expected_returns = T.tensor(self.state_vals, dtype=T.float) + advantages
+        advantages = normalize(advantages)
+
+        aux_advatages = calculate_advantages(
+            self.rewards,
+            self.aux_state_values,
+            self.dones,
+            config['discount_factor'],
+            config['gae_lambda']
+        )
+
+        aux_returns = T.tensor(self.aux_state_values,
+                               dtype=T.float) + aux_advatages
+
+        self.expected_returns.extend(expected_returns)
+        self.advantages.extend(advantages)
+        self.aux_rets.extend(aux_returns)
 
     def __getitem__(self, index):
         state = self.states[index]
