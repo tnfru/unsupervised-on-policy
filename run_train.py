298c8d6ef474af41bed493d6394687e2265bd415
Lars Müller <lars.lmueller@gmail.com>
2022-02-12 21:19:39 +0100

    update: settings for atari

--- run_train.py
+++ run_train.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
               'temperature': 0.1,
               'frames_to_skip': 4,
               'stacked_frames': 4,
-              'is_pretrain': True,
+              'is_pretrain': False,
               'steps_before_repr_learning': 1600,  # Paper value
               'replay_buffer_size': 10000,
               'num_envs': 16,  # Parallel Envs
