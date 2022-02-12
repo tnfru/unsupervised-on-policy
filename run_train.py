0a4b1c760dc2b1e6e6bf226eaa09b3101de3d4d6
Lars Müller <lars.lmueller@gmail.com>
2022-02-12 23:26:34 +0100

    update: pretrain settings

--- run_train.py
+++ run_train.py
@@ -28,11 +28,11 @@ if __name__ == '__main__':
               'temperature': 0.1,
               'frames_to_skip': 4,
               'stacked_frames': 4,
-              'is_pretrain': False,
+              'is_pretrain': True,
               'steps_before_repr_learning': 1600,  # Paper value
               'replay_buffer_size': 10000,
               'num_envs': 16,  # Parallel Envs
-              'prefix': 'ATARI'
+              'prefix': 'PRETRAIN'
               }
 
     if config['is_pretrain']:
