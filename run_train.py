95582d0ff110d9aebbd7da83ef0b239e4be19521
Lars Müller <lars.lmueller@gmail.com>
2022-02-26 20:03:17 +0100

    update: entropy const

--- run_train.py
+++ run_train.py
@@ -5,14 +5,14 @@ import pretrain.environment as environment
 if __name__ == '__main__':
     config = {'policy_clip': 0.25,
               'kl_max': None,  # 0.05 used previously
-              'kl_max_aux': 0.01,  # stability
+              'kl_max_aux': None,  # stability in pretrain 0.01
               'clip_reward': True,
               'beta': 1,
               'val_coeff': 1e-2,
               'train_iterations': 1,
               'entropy_coeff': 0.01,
-              'entropy_min': 0.001,
-              'entropy_decay': 0.9997,
+              'entropy_min': 0.01,  # 0.005 alt
+              'entropy_decay': 0.9999,
               'grad_norm': 10,
               'grad_norm_ppg': 0.5,
               'critic_lr': 1e-3,
@@ -44,6 +44,7 @@ if __name__ == '__main__':
             'target_batch_size': 512,
             'entropy_min': 0.01,
             'actor_lr': 1e-4,  # Paper val 1e-4 while pre-Training
+            'kl_max_aux': 0.01,  # stability in pretrain
         })
 
     config.update({
